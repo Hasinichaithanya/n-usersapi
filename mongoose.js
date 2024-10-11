4265d416fe56e4bc8a3f323ba90b8bb3516d55e3
Chaithanya Hasini Doopati <109410706+Hasinichaithanya@users.noreply.github.com>
2024-10-11 15:12:26 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const user = new Schema({
-  name: String,
-  mail: String,
-  password: String,
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minLength: [3, "Name should be atleast 3 characters"],
+    maxLength: [25, "Name should not exceed 25 characters"],
+  },
+  mail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (mail) {
+        return mail.match(emailRegex);
+      },
+      message: "Please provide valid e-mail address",
+    },
+  },
+  password: {
+    type: String,
+    minLength: [8, "password must be 8 characters long!"],
+  },
 });
 
 const User = model("User", user);
