b401000480abfef247edab1127fa580d55a08e62
Chaithanya Hasini Doopati <hasinichaithanya04@gmail.com>
2024-10-11 15:17:47 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const { User, uri } = require("./mongoose.js");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(uri)
